bcfd5bc76ec8cc7f9d90098bf631f99660597444
watanabe takaaki <watanabe_t@waku-2.com>
2018-06-14 12:28:11 +0900

    stylelintを調整

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,17 +2,15 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recess-order',
-  ],
-  plugins: [
-    'stylelint-order',
+    'stylelint-config-prettier',
   ],
   rules: {
-    'order/properties-order': require('stylelint-config-idiomatic-order').rules['order/properties-order'],
-    'selector-pseudo-class-no-unknown': [true, {
-      ignorePseudoClasses: ['global'],
-    }],
-    'at-rule-no-unknown': [true, {
-      ignoreAtRules: ['/.*/'],
-    }],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+    'at-rule-no-unknown': null,
   },
 };
